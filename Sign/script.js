4b88125fc888b5b678009da9132b9a0e54249f9d
Matheus Gabriel <math.gsilva07@gmail.com>
2023-11-19 16:37:25 -0300

    add navbar css

--- Sign/script.js
+++ Sign/script.js
@@ -1,16 +1,19 @@
-document
-        .querySelector("form")
-        .addEventListener("submit", function (event) {
-          event.preventDefault();
-          document.querySelector(".root").classList.add("fade-out");
-          setTimeout(function () {
-            document.querySelector(".root").classList.remove("fade-out");
+/*  LOADING ANIMATION SCRIPT
+    UTA: 19/11/23
+    @author Matheus Gabriel
+*/
 
-            document.querySelector(".root").innerHTML =
-              "<div class='lds-dual-ring'></div><h1 class='loding'>Carregando...</h1>";
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  document.querySelector(".root").classList.add("fade-out");
+  setTimeout(function () {
+    document.querySelector(".root").classList.remove("fade-out");
 
-            setTimeout(function () {
-              window.location.href = event.target.action;
-            }, 5000); 
-          }, 500);
-        });
+    document.querySelector(".root").innerHTML =
+      "<div class='lds-dual-ring'></div><h1 class='loding'>Carregando...</h1>";
+
+    setTimeout(function () {
+      window.location.href = event.target.action;
+    }, 5000);
+  }, 500);
+});
