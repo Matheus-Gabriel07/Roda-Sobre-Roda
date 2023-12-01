57af0fb5192d1a509bf94eb584bac838fcddbe95
Matheus-Gabriel07 <math.gsilva07@gmail.com>
2023-12-01 13:52:54 -0300

    update css

---
+++ scripts/loading.js
@@ -0,0 +1,14 @@
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  document.querySelector(".root").classList.add("fade-out");
+  setTimeout(function () {
+    document.querySelector(".root").classList.remove("fade-out");
+
+    document.querySelector(".root").innerHTML =
+      "<div class='lds-dual-ring'></div><h1 class='loding'>Carregando...</h1>";
+
+      setTimeout(function () {
+        window.location.href = event.target.action;
+      }, 5000);
+  }, 500);
+});
